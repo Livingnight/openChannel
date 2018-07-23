310b3b121fb4189ddc5446745f97091c63254339
Vanessa Borras <vborras@AT-CNN-GFX-MB04.turner.com>
2018-07-23 18:41:03 -0400

    added component Nav folder

---
+++ client/src/components/Nav/Nav.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+import './Nav.css';
+import logo from "../../Images/openChannel_logo.png";
+// import './App.css';
+
+
+
+export default class Nav extends Component {
+    goTo(route) {
+        this.props.history.replace(`/${route}`)
+    }
+
+
+    login() {
+        this.props.auth.login();
+    }
+
+    logout() {
+        this.props.auth.logout();
+    }
+
+    render() {
+        const { isAuthenticated } = this.props.auth;
+
+        return (
+
+            <div>
+                {console.log(this.props)}
+                <Navbar className= "Navbar" fluid>
+                    {/* <Navbar.Header> */}
+                        <Navbar.Brand>
+                            <a href="/"><img className="logo" src={logo}/></a>
+                        </Navbar.Brand>
+                        <div className= "nav-buttons">
+                        <Button
+                            bsStyle="link"
+                            className="btn-margin"
+                            onClick={this.goTo.bind(this, 'goals')}
+                        >
+                            Goals
+                        </Button>
+                        <Button
+                            bsStyle="link"
+                            className="btn-margin"
+                            onClick={this.goTo.bind(this, 'goalItems')}
+                        >
+                            Goal Items
+                        </Button>
+                        {
+                            !isAuthenticated() && (
+                                <Button
+                                    id="qsLoginBtn"
+                                    // bsStyle="link"
+                                    className="btn-margin"
+                                    onClick={this.login.bind(this)}
+                                >
+                                    Log In
+                                </Button>
+                            )
+                            
+                        }
+                        {
+                            isAuthenticated() && (
+                                <Button
+                                id="qsLogoutBtn"
+                                bsStyle="link"
+                                className="btn-margin"
+                                onClick={this.logout.bind(this)}
+                                >
+                                    Goals
+                                </Button>
+                            )
+                        }
+                        </div>
+                    {/* </Navbar.Header> */}
+                </Navbar>
+            </div>
+
+
+        );
+    }
+}
